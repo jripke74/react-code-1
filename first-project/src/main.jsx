621d1f73fbda1aa35356a7ed91c60f7c1f9369e3
Jeff Ripke <jripke74@gmail.com>
2025-02-05 14:27:21 -0800

    init commit

---
+++ first-project/src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root")).render(
+  <ul>
+    <li>Super popular JS library</li>
+    <li>Will help me be even more employable</li>
+    <li>React has a pretty cool logo</li>
+  </ul>
+);
