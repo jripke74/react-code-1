e963a90a04148ed2ac3d0222946f4c57ea335906
Jeff Ripke <jripke74@gmail.com>
2025-02-06 14:07:38 -0800

    add Header, Main, and Footer

--- first-project/src/main.jsx
+++ first-project/src/main.jsx
@@ -1,26 +1,39 @@
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
-root.render(
-  <Page />
-);
+
+function Header() {
+  return (
+      <header>
+        <img src="/src/assets/react.svg" alt="React Logo"/>
+      </header>
+  )
+}
+
+function Main() {
+  return (
+    <main>
+      <h1>Reason I am excited to learn React</h1>
+      <ol>
+        <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+        <li>I am more likely to get a job as a front end developer if I know React</li>
+      </ol>
+    </main>
+  )
+}
+
+function Footer() {
+  return <small>&copy; 2025 Jeff development. All rights reserved.</small>
+}
 
 function Page() {
   return (
     <>
-      <header>
-        <img src="/src/assets/react.svg" alt="React Logo" />
-      </header>
-      <main>
-        <h1>Reasons to be excited about React</h1>
-        <ol>
-          <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
-          <li>I am more likely to get a job as a front end developer if I know React</li>
-        </ol>
-      </main>
-      <footer>
-        <p>&copy; 2025 Ripke development. All rights reserved.</p>
-      </footer>
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
+
+root.render(<Page />)
