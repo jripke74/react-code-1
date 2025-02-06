bbc5c019ecaaea92673502163d973861ef75d715
Jeff Ripke <jripke74@gmail.com>
2025-02-06 00:42:24 -0800

    add React fun facts

--- first-project/src/main.jsx
+++ first-project/src/main.jsx
@@ -1,12 +1,27 @@
-import {createElement } from "react";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <main>
-    <img src="/src/assets/react.svg" alt="React logo"/>
-    <h1>This is another element</h1>
+    <img src="/src/assets/react.svg" alt="React Logo" width="40px" />
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first release in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 200K stars on GitHub</li>
+      <li>Is maintained by Meta</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
   </main>
+)
 
-);
+// import { createRoot } from "react-dom/client";
+//
+// const root = createRoot(document.getElementById("root"));
+// root.render(
+//   <main>
+//     <img src="/src/assets/react.svg" alt="React logo"/>
+//     <h1>This is another element</h1>
+//   </main>
+// );
 
