cf8b5aef738fb0a2317eb99af8c4636e5cf0e3e4
Jeff Ripke <jripke74@gmail.com>
2025-02-06 09:27:28 -0800

    add TemporaryName()

--- first-project/src/main.jsx
+++ first-project/src/main.jsx
@@ -2,26 +2,21 @@ import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <main>
-    <img src="/src/assets/react.svg" alt="React Logo" width="40px" />
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first release in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 200K stars on GitHub</li>
-      <li>Is maintained by Meta</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </main>
+  <TemporaryName />
 )
 
-// import { createRoot } from "react-dom/client";
-//
-// const root = createRoot(document.getElementById("root"));
-// root.render(
-//   <main>
-//     <img src="/src/assets/react.svg" alt="React logo"/>
-//     <h1>This is another element</h1>
-//   </main>
-// );
-
+function TemporaryName() {
+  return (
+    <main>
+      <img src="/src/assets/react.svg" alt="React Logo" width="40px" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first release in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 200K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  )
+}
