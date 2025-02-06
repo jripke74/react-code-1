1abd938445a491cf0634dbf0d073c303ea94d437
Jeff Ripke <jripke74@gmail.com>
2025-02-06 10:45:27 -0800

    add Page()

--- first-project/src/main.jsx
+++ first-project/src/main.jsx
@@ -2,21 +2,30 @@ import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <TemporaryName />
-)
+  <Page />
+);
 
-function TemporaryName() {
+function Page() {
   return (
-    <main>
-      <img src="/src/assets/react.svg" alt="React Logo" width="40px" />
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first release in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 200K stars on GitHub</li>
-        <li>Is maintained by Meta</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-    </main>
+    <ol>
+      <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+      <li>I am more likely to get a job as a front end developer if I knkow React</li>
+    </ol>
   )
 }
+
+// function Page() {
+//   return (
+//     <main>
+//       <img src="/src/assets/react.svg" alt="React Logo" width="40px" />
+//       <h1>Fun facts about React</h1>
+//       <ul>
+//         <li>Was first release in 2013</li>
+//         <li>Was originally created by Jordan Walke</li>
+//         <li>Has well over 200K stars on GitHub</li>
+//         <li>Is maintained by Meta</li>
+//         <li>Powers thousands of enterprise apps, including mobile apps</li>
+//       </ul>
+//     </main>
+//   )
+// }
